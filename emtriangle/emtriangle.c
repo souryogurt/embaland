82f5e9dd423347cd414f02a200f127a09aa3348b
Egor Artemov <egor.artemov@gmail.com>
2019-08-04 05:33:44 +0300

    Initialize glfw library

--- emtriangle/emtriangle.c
+++ emtriangle/emtriangle.c
@@ -10,6 +10,7 @@
 
 #include "emtriangle.h"
 
+#include <GLFW/glfw3.h>
 #include <embaland/embaland.h>
 
 static emb_instance emb;
@@ -17,11 +18,17 @@ static emb_instance emb;
 int application_main(int argc, char **argv)
 {
 	int retval = EXIT_SUCCESS;
-	if (emb_create(&emb) != EMB_SUCCESS) {
+	if (!glfwInit()) {
 		retval = EXIT_FAILURE;
 		goto exit;
 	}
+	if (emb_create(&emb) != EMB_SUCCESS) {
+		retval = EXIT_FAILURE;
+		goto glfw_terminate;
+	}
 	emb_destroy(emb);
+glfw_terminate:
+	glfwTerminate();
 exit:
 	return retval;
 }
